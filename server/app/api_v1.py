3a836655f30523ac33c183c4bc75eba693829084
Yaogang Lian <yaogang.lian@gmail.com>
2013-03-14 11:39:57 -0400

    Integrate with TransLoc.

--- server/app/api_v1.py
+++ server/app/api_v1.py
@@ -13,45 +13,73 @@ from pyquery import PyQuery
 #
 
 class PredictionHandler(webapp.RequestHandler):
-	@memcached (30)
-	def get(self):
-		stopNumber = self.request.get('stop')
-		url = "http://avlweb.charlottesville.org/RTT/Public/RoutePositionET.aspx?PlatformNo=%s&Referrer=uvamobile" % (stopNumber)
-        
-		result = urlfetch.fetch(url, deadline=10)
-		if result.status_code == 200:
-		    S = PyQuery(PyQuery(result.content).html())
-		else:
-		    self.error(500)
-		
-		result = []
-		for el in S('table.tableET tbody tr'):
-		    count = 0
-		    entry = {}
-		    trTag = S(el)
-		    for ell in trTag.find('td'):
-		        tdTag = S(ell)
-		        if count == 0:
-		            entry['route'] = tdTag.text()
-		        elif count == 1:
-		            dest = tdTag.text()
-		            note = None
-		            matchObj = re.search("(via .*)", dest)
-		            if matchObj is not None:
-		                note = matchObj.group(1)
-		            else:
-		                matchObj = re.search("(to .*)", dest)
-		                if matchObj is not None:
-		                    note = matchObj.group(1)
-		            if note is not None:
-		                entry["note"] = note
-		        elif count == 2:
-		            entry['eta'] = tdTag.text()
-		        count = count + 1
-		    
-		    if entry:
-		        result.append(entry)
-		return result
+    @memcached (30)
+    def get(self):
+	    stopNumber = self.request.get('stop')
+	    predictions = []
+	    
+	    # Make asynchronous request to the Connexionz system.
+	    rpc1 = urlfetch.create_rpc()
+	    urlfetch.make_fetch_call(rpc1, "http://avlweb.charlottesville.org/RTT/Public/RoutePositionET.aspx?PlatformNo=%s&Referrer=uvamobile" % (stopNumber))
+	    
+	    # Make asynchronous request to the TransLoc system.
+	    rpc2 = urlfetch.create_rpc()
+	    urlfetch.make_fetch_call(rpc2, "http://vcu.transloc.com/m/stop/code/117")
+	    
+	    # Check result from Connexionz
+	    try:
+	        result1 = rpc1.get_result()
+	        if result1.status_code == 200:
+	            S = PyQuery(PyQuery(result1.content).html())
+	            for el in S('table.tableET tbody tr'):
+	                count = 0
+	                entry = {}
+	                trTag = S(el)
+	                for ell in trTag.find('td'):
+	                    tdTag = S(ell)
+	                    if count == 0:
+	                        entry['route'] = tdTag.text()
+	                    elif count == 1:
+	                        dest = tdTag.text()
+	                        note = None
+	                        matchObj = re.search("(via .*)", dest)
+	                        if matchObj is not None:
+	                            note = matchObj.group(1)
+	                        else:
+	                            matchObj = re.search("(to .*)", dest)
+	                            if matchObj is not None:
+	                                note = matchObj.group(1)
+	                        if note is not None:
+	                            entry["note"] = note
+	                    elif count == 2:
+	                        entry['eta'] = tdTag.text()
+	                    count = count + 1
+	                
+	                if entry:
+	                    predictions.append(entry)
+	    except urlfetch.DownloadError:
+	        # Request time out or failed
+	        logging.error("Request time out or failed for Connexionz.")
+	    
+	    # Check result from TransLoc
+	    try:
+	        result2 = rpc2.get_result()
+	        if result2.status_code == 200:
+	            S = PyQuery(result2.content)
+	            for el in S('ul#routes li'):
+	                entry = {}
+	                rowTag = S(el)
+	                etaTag = rowTag.find('.wait_time')
+	                logging.info(etaTag.text())
+	                
+	                if entry:
+	                    predictions.append(entry)
+	    except urlfetch.DownloadError:
+	        # Request time out or failed
+	        logging.error("Request time out or failed for TransLoc.")
+	    
+	    # Return combined results from two systems.
+	    return predictions
 
 #
 # Router
--- server/todo.md
+++ server/todo.md
@@ -1,4 +1,6 @@
 * Move routes and predictions to my server
 * Measure server traffic and cost
+* Integrate with new Transloc system
+
 * Client enhancements: edit favorite, route overlay, router lookup
 * Test alarm feature

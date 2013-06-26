17beb11f92836ecc36f09d768eda6ca7e8086f8d
Yaogang Lian <yaogang.lian@gmail.com>
2013-06-26 17:45:36 -0400

    Prep for open source.

--- server/fix_path.py
+++
@@ -1,9 +0,0 @@
-"""
-Add directories to the module search path.
-"""
-
-import os
-import sys
-
-sys.path.append(os.path.join(os.path.dirname(__file__), 'app'))
-sys.path.append(os.path.join(os.path.dirname(__file__), 'vendor'))
--- server/todo.md
+++
@@ -1,6 +0,0 @@
-* Move routes and predictions to my server
-* Measure server traffic and cost
-* Integrate with new Transloc system
-
-* Client enhancements: edit favorite, route overlay, router lookup
-* Test alarm feature
